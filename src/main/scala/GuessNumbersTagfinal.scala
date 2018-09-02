af86fdccc6f530d11cfd3c711235d81e54a16c40
jesus <jesusmluque@gmail.com>
2018-09-02 22:10:07 +0200

    Random numbers as a type class

--- src/main/scala/GuessNumbersTagfinal.scala
+++ src/main/scala/GuessNumbersTagfinal.scala
@@ -24,6 +24,11 @@ object GuessNumbersTagfinal {
   def putStrLn[F[_]: Console](str: String) = implicitly[Console[F]].putStrLn(str)
   def getStrLn[F[_]: Console] = implicitly[Console[F]].getStrLn
 
+  trait RandomNum[F[_]] {
+    def nextInt(l: Int): F[Int]
+  }
+  def nextInt[F[_]: RandomNum](l: Int) = implicitly[RandomNum[F]].nextInt(l)
+
   def parseInt(str: String): Option[Int] = Try(str.toInt) match {
     case Failure(_) => None
     case Success(value) => Some(value)
@@ -39,24 +44,21 @@ object GuessNumbersTagfinal {
 
   } yield cont
 
-  def game[F[_]: Monad: Console](name: String): F[Unit] = {
-    val num = scala.util.Random.nextInt(5) + 1
+  def game[F[_]: Monad: RandomNum: Console](name: String): F[Unit] = for {
+    num <- nextInt(5)
+    _ <- putStrLn("Di un numero: ")
+    guess <- getStrLn
+    _ <- parseInt(guess) match {
+      case None => putStrLn("Lo siento no has introducido un número")
+      case Some(value) if value == num => putStrLn(s"Felicidades $name, el número $value es correcto")
+      case Some(_) => putStrLn(s"Lo siento $name, no es correcto")
+    }
+    cont <- checkContinue
+    _ <- if (cont) game(name) else putStrLn("Gracias por jugar, adios")
 
-    for {
-      _ <- putStrLn("Di un numero: ")
-      guess <- getStrLn
-      _ <- parseInt(guess) match {
-        case None => putStrLn("Lo siento no has introducido un número")
-        case Some(value) if value == num => putStrLn(s"Felicidades $name, el número $value es correcto")
-        case Some(_) => putStrLn(s"Lo siento $name, no es correcto")
-      }
-      cont <- checkContinue
-      _ <- if (cont) game(name) else putStrLn("Gracias por jugar, adios")
+  } yield ()
 
-    } yield ()
-  }
-
-  def Program[F[_]: Monad: Console]: F[Unit] =
+  def Program[F[_]: Monad: RandomNum: Console]: F[Unit] =
     for {
       _ <-   putStrLn("Nombre: ")
       name <- getStrLn
@@ -80,6 +82,9 @@ object GuessNumbersTagfinal {
       override def putStrLn(str: String): IO[Unit] = IO(() => println(str))
 
       override def getStrLn: IO[String] = IO(() => readLine())
+    }
+    implicit val randomIO = new RandomNum[IO] {
+      override def nextInt(l: Int): IO[Int] = IO(() => scala.util.Random.nextInt(l) + 1)
     }
   }
   def main(args: Array[String]): Unit = Program[IO].performEff()
